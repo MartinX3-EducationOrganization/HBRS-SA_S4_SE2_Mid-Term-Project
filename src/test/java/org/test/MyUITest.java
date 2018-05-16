a5629ca280dab165fd32f938db7c62bd53fad89d
Martin Duenkelmann <martin.duenkelmann@smail.inf.h-brs.de>
2018-05-16 12:56:30 +0200

    Add JUnit 4.12

---
+++ src/test/java/org/test/MyUITest.java
@@ -0,0 +1,31 @@
+/*
+ * Created by Martin Dünkelmann on 16.05.18 12:52
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 16.05.18 12:52
+ */
+
+package org.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author martin on 16.05.18
+ * @project wi-inf_se2_2018_grundgeruest
+ */
+public class MyUITest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void init() {
+    }
+}
