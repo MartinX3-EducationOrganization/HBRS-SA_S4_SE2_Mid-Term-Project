44afcb53798cdfe47d9c65dbc2ffd4face801548
Martin Duenkelmann <martin.duenkelmann@smail.inf.h-brs.de>
2018-05-24 22:38:30 +0200

    fixed delay
    update
    
    http://10.20.129.92:8080/browse/GRUN-134
    
    Central View with Nested Views and a Singleton Navigator

--- src/main/java/org/bonn/se/ss18/view/Login.java
+++ src/main/java/org/bonn/se/ss18/view/Login.java
@@ -20,27 +20,76 @@ public class Login extends Panel implements View {
     public static final String NAME = "";
 
     public Login() {
-        setContent(new HorizontalLayout(setLayoutLeft(), setLayoutCentre(), setlayoutRight()));
+        HorizontalLayout content = setSiteLayout();
+
+        content.setSizeFull();
+        
+        setContent(content);
+    }
+
+    private HorizontalLayout setSiteLayout() {
+        VerticalLayout layoutLeft = setLayoutLeft();
+        VerticalLayout layoutCentre = setLayoutCentre();
+        VerticalLayout layoutRight = setLayoutRight();
+
+        layoutLeft.setSizeUndefined();
+        layoutCentre.setSizeFull();
+        layoutRight.setSizeUndefined();
+
+        HorizontalLayout layout = new HorizontalLayout(
+                layoutLeft,
+                layoutCentre,
+                layoutRight
+        );
+
+        layout.setComponentAlignment(layoutLeft, Alignment.MIDDLE_LEFT);
+        layout.setComponentAlignment(layoutCentre, Alignment.MIDDLE_CENTER);
+        layout.setComponentAlignment(layoutRight, Alignment.MIDDLE_RIGHT);
+
+        return layout;
     }
 
     private VerticalLayout setLayoutCentre() {
-        return new VerticalLayout();
+        return new VerticalLayout(
+                new Label("Herzlich Willkommen auf Coll@HBRS"),
+                setLayoutCentreVertical()
+        );
+    }
+
+    private VerticalLayout setLayoutCentreVertical() {
+        return new VerticalLayout(
+                new Label("Anmeldung"),
+                new Label("Bitte geben Sie ihren Benutzernamen und ihr Passwort ein"),
+                setLayoutCentreForm()
+        );
+    }
+
+    private FormLayout setLayoutCentreForm() {
+        return new FormLayout(
+                new TextField("Benutzername"),
+                new TextField("Passwort")
+        );
     }
 
     private VerticalLayout setLayoutLeft() {
         Image logo = new Image(
                 "",
-                new FileResource(new File(VaadinService.getCurrent().getBaseDirectory().getAbsolutePath() + "/../resources/logo.png")));
+                new FileResource(new File(VaadinService.getCurrent().getBaseDirectory().getAbsolutePath() + "/../resources/logo.png"))
+        );
+
         logo.setHeight("100");
         logo.setWidth("100");
+
         return new VerticalLayout(logo);
     }
 
-    private VerticalLayout setlayoutRight() {
+    private VerticalLayout setLayoutRight() {
         return new VerticalLayout(
                 new Label("Noch nicht registriert?"),
                 new Button("Jetzt registrieren!", //TODO: Login -> Registration
-                        event -> UI.getCurrent().getNavigator().navigateTo("Registration")));
+                        event -> UI.getCurrent().getNavigator().navigateTo("Registration")
+                )
+        );
     }
 
     @Override
