e46fb915295b6d23c2fab3e9138aecce894b7e59
David Zirwes <david.zirwes@smail.inf.h-brs.de>
2018-06-10 00:05:29 +0200

    AnzeigeDAO SQL

--- src/main/java/org/bonn/se/ss18/dao/AnzeigeDAO.java
+++ src/main/java/org/bonn/se/ss18/dao/AnzeigeDAO.java
@@ -46,10 +46,12 @@ public class AnzeigeDAO extends GenericDAO<Anzeige> {
         return null;
     }
 
-    @Override
     public boolean create(Anzeige stelle) {
         try {
-            PreparedStatement ps = con.prepareStatement("INSERT INTO " + tableName + " VALUES (?, ?, ?, ?, ?,?,?,?,?,?,?,?)");
+            PreparedStatement ps = con.prepareStatement("INSERT INTO " + tableName + " "
+                        + "VALUES (null, " + stelle.getUserid() + ", " + stelle.getDatum() + ", " + stelle.getTitel() + ", " + stelle.getOrt() + ", "
+                            + stelle.getTyp() + ", " + stelle.getAnstellungsart() + ", " + stelle.getArbeitszeit() + ", " + stelle.getBrancheid() + ", "
+                            + stelle.getBeginn() + ", " + stelle.getAktiv() + ", " + stelle.getText() + ")");
             return createps(stelle, ps);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -58,10 +60,13 @@ public class AnzeigeDAO extends GenericDAO<Anzeige> {
         return false;
     }
 
-    @Override
     public boolean update(Anzeige stelle) {
         try {
-            PreparedStatement ps = con.prepareStatement("UPDATE " + tableName + " SET anzeigeid=?,userid=?,datum=?,titel=?,ort=?,typ=?,anstellungsart=?,arbeitszeit=?,bracheid=?,beginn=?,aktiv=?,text=? WHERE id=" + stelle.getId());
+            PreparedStatement ps = con.prepareStatement("UPDATE " + tableName + " "
+                + "SET userid = " + stelle.getUserid() + ", datum = " + stelle.getDatum() + ", titel = " + stelle.getTitel() + ", ort = " + stelle.getOrt() + ", "
+                            + "typ = " + stelle.getTyp() + ", anstellungsart = " + stelle.getAnstellungsart() + ", arbeitszeit = " + stelle.getArbeitszeit() + ", " 
+                            + "brancheid = " + stelle.getBrancheid() + ", beginn = " + stelle.getBeginn() + ", aktiv = " + stelle.getAktiv() + ", text = " + stelle.getText()
+                + "WHERE id = " + stelle.getId());
             return createps(stelle, ps);
 
         } catch (SQLException ex) {
@@ -88,14 +93,14 @@ public class AnzeigeDAO extends GenericDAO<Anzeige> {
         ps.setInt(1, anzeige.getId());
         ps.setInt(2, anzeige.getUserid());
         ps.setDate(3, Date.valueOf(anzeige.getDatum()));
-        ps.setString(4, anzeige.getTitle());
+        ps.setString(4, anzeige.getTitel());
         ps.setString(5, anzeige.getOrt());
         ps.setString(6, anzeige.getTyp());
         ps.setString(7, anzeige.getAnstellungsart());
         ps.setString(8, anzeige.getArbeitszeit());
-        ps.setInt(9, anzeige.getBranchenid());
+        ps.setInt(9, anzeige.getBrancheid());
         ps.setDate(10, Date.valueOf(anzeige.getBeginn()));
-        ps.setBoolean(11, anzeige.isAktiv());
+        ps.setBoolean(11, anzeige.getAktiv());
         ps.setString(12, anzeige.getText());
 
         int i = ps.executeUpdate();
@@ -109,12 +114,12 @@ public class AnzeigeDAO extends GenericDAO<Anzeige> {
         anzeige.setId(rs.getInt("anzeigeid"));
         anzeige.setUserid(rs.getInt("userid"));
         anzeige.setDatum(rs.getDate("datum"));
-        anzeige.setTitle(rs.getString("titel"));
+        anzeige.setTitel(rs.getString("titel"));
         anzeige.setOrt(rs.getString("ort"));
         anzeige.setTyp(rs.getString("typ"));
         anzeige.setAnstellungsart(rs.getString("anstellungsart"));
         anzeige.setArbeitszeit(rs.getString("arbeitszeit"));
-        anzeige.setBranchenid(rs.getInt("brancheid"));
+        anzeige.setBrancheid(rs.getInt("brancheid"));
         anzeige.setBeginn(rs.getDate("beginn"));
         anzeige.setAktiv(rs.getBoolean("aktiv"));
         anzeige.setText(rs.getString("text"));
